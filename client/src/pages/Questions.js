3d00fdec03046bf1079ac587e1879817adcbecd1
Carla <carla@layc-dc.org>
2021-01-16 13:05:06 -0500

    added cards for score, first question, and timer

--- client/src/pages/Questions.js
+++ client/src/pages/Questions.js
@@ -1,12 +1,26 @@
 import React from "react";
 import Container from "../components/Container";
-import Card from "../components/Card";
 import Jumbotron from "../components/Jumbotron";
 import Row from "../components/Row";
 import Button from "../components/Button";
+import Col from "../components/Col";
 
 function Questions() {
-
+    return (
+        <Container>
+            <Row>
+               <div className="card-body">
+                   <h3>Scores</h3>
+                </div> 
+                <div className="card-body">
+                   <h3>Question 1 of 1</h3>
+                </div> 
+                <div className="card-body">
+                   <h3>Timer</h3>
+                </div> 
+            </Row>
+        </Container>
+    )
 }
 
 export default Questions;
