a9c1ea6e5f251f160644fe6c2779485a1d6c5341
Carla <carla@layc-dc.org>
2021-01-16 13:43:39 -0500

    hard-coded answer choices

--- client/src/pages/Questions.js
+++ client/src/pages/Questions.js
@@ -9,16 +9,41 @@ function Questions() {
     return (
         <Container>
             <Row>
-               <div className="card-body">
-                   <h3>Scores</h3>
-                </div> 
                 <div className="card-body">
-                   <h3>Question 1 of 1</h3>
-                </div> 
+                    <h3>Score</h3>
+                </div>
                 <div className="card-body">
-                   <h3>Timer</h3>
-                </div> 
+                    <h3>Question 1 of 1</h3>
+                </div>
+                <div className="card-body">
+                    <h3>Timer</h3>
+                </div>
             </Row>
+            <Jumbotron>
+                <h1 class="styling">Which country does this flag belong to?</h1>
+            </Jumbotron>
+            <Col>
+                <Row>
+                    <div className="card-body">
+                        <Button>Argentina</Button>
+                    </div>
+                </Row>
+                <Row>
+                    <div className="card-body">
+                        <Button>South Africa</Button>
+                    </div>
+                </Row>
+                <Row>
+                    <div className="card-body">
+                        <Button>Germany</Button>
+                    </div>
+                </Row>
+                <Row>
+                    <div className="card-body">
+                        <Button>Canada</Button>
+                    </div>
+                </Row>
+            </Col>
         </Container>
     )
 }
