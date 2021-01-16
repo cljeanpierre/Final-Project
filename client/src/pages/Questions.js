a3a01dcec1d9d4bb73e7df31fd0eeeb9e542d860
Carla <carla@layc-dc.org>
2021-01-16 12:32:06 -0500

    added questions page and imported components

---
+++ client/src/pages/Questions.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "../components/Container";
+import Card from "../components/Card";
+import Jumbotron from "../components/Jumbotron";
+import Row from "../components/Row";
+import Button from "../components/Button";
+
+function Questions() {
+
+}
+
+export default Questions;
