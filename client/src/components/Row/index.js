19923ce435c416bb522315f17304d608a32ca323
Dina Saunders <dina_saunders@mac.com>
2021-01-23 14:10:43 -0500

    updated quiz page layout fixed flag and choice displays

--- client/src/components/Row/index.js
+++ client/src/components/Row/index.js
@@ -4,10 +4,7 @@ import Radium from "radium";
 function Row(props) {
     const styles = {
         base: {
-            paddingTop: '10px',
-            paddingLeft:'10px',
-            paddingRight:'10px',
-            paddingBottom: '10px',
+            padding: '0',
             background: '#cfdac8',
             textAlign: 'center',
             marginTop: '5px',
@@ -15,6 +12,7 @@ function Row(props) {
             marginRight:'5px',
             marginBottom: '20px',
             borderRadius: '15px',
+            
         }
     }
     return <div className="row" style={styles.base}>{props.children}</div>
